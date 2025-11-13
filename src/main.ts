e5bae72e11399cd48e5feefca7929d4b7a278bfa
Freddy <freddy990117@gmail.com>
2025-11-13 10:35:45 +0800

    1.設定 TS Interface 取得需要的資訊 (Name,Phone,Email,Job) 2.將 HTML 的 id 全數更改為小寫

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+interface Person {
+  Number: number;
+  Name: string;
+  Job: string;
+  Email: string;
+}
+const personNumber = document.getElementById("phone") as HTMLInputElement;
+const personName = document.getElementById("name") as HTMLInputElement;
+const personJob = document.getElementById("job") as HTMLInputElement;
+const personEmail = document.getElementById("email") as HTMLInputElement;
+
+const Person1: Person = {
+  Number: Number(personNumber),
+  Name: personName.value,
+  Job: personJob.value,
+  Email: personEmail.value,
+};
+
+console.log(Person1);
