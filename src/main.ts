ebcb78d529d9bc6128295233c0b0342ff3fb5b33
Freddy <freddy990117@gmail.com>
2025-11-30 08:48:59 +0800

    設定一個 messageValue 參數來提取 Input Value

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,37 @@
 //   Email: string;
 //   Message: string;
 // }
-
+const checkboxArr: Array<String> = [];
+// 需要重新賦值所以使用 let
+let messageValue: string = "";
 const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("click btn");
-
+  // 取得 Textarea 輸入框
+  const personMessage = document.getElementById(
+    "message"
+  ) as HTMLTextAreaElement;
+  // 取得所有的 checkbox
   const chechBoxes =
     document.querySelectorAll<HTMLInputElement>(".form-check-input");
+  //  跑迴圈遍布 checkbox
   chechBoxes.forEach((box) => {
-    if (box.checked) {
-      console.log(box.name);
+    // 是否有 check
+    if (box?.checked) {
+      // 將有勾選的選項 push 到 array 當中
+      checkboxArr.push(box.name);
     }
-    const personMessage = document.getElementById(
-      "message"
-    ) as HTMLInputElement;
-    console.log(personMessage.value);
   });
+  console.log(checkboxArr);
+  messageValue = personMessage.value;
+  console.log(messageValue);
 });
+
 // 透過 id 取得 HTML 中的值
 // const personPhone = document.getElementById(".phone") as HTMLInputElement;
 // const personName = document.getElementById("name") as HTMLInputElement;
 // const personJob = document.getElementById("job") as HTMLInputElement;
 // const personEmail = document.getElementById("email") as HTMLInputElement;
-
 // const Person1: Person = {
 //   Phone: Number(personPhone),
 //   Name: personName.value,
