3a2a0c5a9fad8c07f0734f7b042f36d693da0617
Freddy990117 <Freddy990117@gmail.com>
2025-12-03 16:13:20 +0800

    1.使用 innerHTML 新增表單 2.新增 personId 與 collapseId 來分別人員與表單

--- src/main.ts
+++ src/main.ts
@@ -20,26 +20,25 @@ const chechBoxes =
 // 需要重新賦值所以使用 let
 let messageValue: string = ""; // 這個是放留言的變數
 let personId: number = 0; // 這個是放每一個表單的 id
-
+let collapseId: number = 0; // 這個是表單內部的 div id
 // 取得按鈕
 const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
 // 按下送出表單會發生的行為
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // 將 Person fn 放置此處並列印確認相同
+  // 每次點擊送出表單，建立一個 person 變數，person 變數內部的資訊由 newPerson 提供
   const person = newPerson();
+  // 隨後會帶入到表單的建立
   newAccording(person);
-  // 賦值給 messageValue
-  // messageValue = personMessage.value;
 });
 
 // 建立一個 Person fn 要符合 Person Interface
 const newPerson = function (): Person {
-  const serviceArr: string[] = [];
   // 設定參數 (原本全域變數更改為區域變數)
   const checkboxArr: Array<string> = [];
 
+  // 跑迴圈確認是否有被點選
   chechBoxes.forEach((box) => {
     // 是否有 check
     if (box?.checked) {
@@ -58,13 +57,54 @@ const newPerson = function (): Person {
   };
 };
 
+// 建立一個 fn 用於「新增」According Item (參數 person 會是使用者輸入的資訊)
 const newAccording = function (person: Person) {
+  // 新增 id 的順序
+  personId++;
+  collapseId++;
+
   // 取得 according 的主幹
   const accrordingContatiner = document.getElementById(
     "accordionExample"
   ) as HTMLDivElement;
-  console.log(accrordingContatiner);
+
+  accrordingContatiner.innerHTML += `
+  <div class="accordion-item">
+              <h2 class="accordion-header" id="${personId}"> 
+                <button
+                  id="collapse-button"
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#${collapseId}"
+                  aria-expanded="true"
+                  aria-controls="${collapseId}"
+                >
+                  Request from : ${person.Name}
+                </button>
+              </h2>
+              <div
+                id="${collapseId}"
+                class="accordion-collapse collapse"
+                aria-labelledby="${personId}"s
+                data-bs-parent="#accordionExample"
+              >
+                <div class="accordion-body" id="collapse-content">
+                  Phone: ${personPhone.value},
+                  <br/>
+                  Email: ${personEmail.value},
+                  <br/>
+                  Job: ${personJob.value},
+                  <br/>
+                  Service: ${person.Service},
+                  <br/>
+                  User want to say : ${person.Message}
+                </div>
+              </div>
+            </div>
+  `;
 };
+
 // //TODO 按下 submit-form 後，一個行為是將資料放入表單中（已完成，但是後續結構需要更改為每個使用者為新的 According Item），一個行為是 依照 id 新增 According Item，而這樣我需要：
 // 1.建立一個 fn 用於存放新的 Person 資訊（要符合 Person 的 interface 結構）
 // 2.建立一個 fn 用於「新增」 According Item （According Item 的 id 從 0 開始 ）
